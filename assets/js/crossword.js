fcbb1dcaeb0337ca0bf5424d21a70c0d905e3b0d
Souradeep <souradeepkrbasu@gmail.com>
2020-06-09 17:33:58 +0530

    Update

--- assets/js/crossword.js
+++ assets/js/crossword.js
@@ -5,45 +5,45 @@ let definition = new CrosswordDefinition({
   width: 8, //width of the board, 5 cells
   acrossClues: [
     {
-      number: 2, //number to identify the world, must be unique
-      x: 1, //The x position where the word starts, starting from 1
-      y: 4, //The y position where the word starts, starting from 1
-      answer: "POPART", //the word itself
-      clue: " First name of the Spanish artist known for 'Las Meninas'.", //the clue
-      // hints: [2],
-    },
-    {
       number: 1, //number to identify the world, must be unique
       x: 4, //The x position where the word starts, starting from 1
       y: 1, //The y position where the word starts, starting from 1
       answer: "DIEGO", //the word itself
-      clue: "Art movement/style championed by Andy Warhol.", //the clue
+      clue: "1. Art movement/style championed by Andy Warhol.", //the clue
       /*hints: [2],*/
+    },
+    {
+      number: 2, //number to identify the world, must be unique
+      x: 1, //The x position where the word starts, starting from 1
+      y: 4, //The y position where the word starts, starting from 1
+      answer: "POPART", //the word itself
+      clue: "2. First name of the Spanish artist known for 'Las Meninas'.", //the clue
+      // hints: [2],
     },
     {
       number: 3,
       x: 1,
       y: 7,
       answer: "VERMEER",
-      clue: "17th century Dutch painter,  known for his luminous depiction of everyday life and ordinary subjects.",
+      clue: "3. 17th century Dutch painter,  known for his luminous depiction of everyday life and ordinary subjects.",
       /* hints: [2, 7] */
     }
   ],
   downClues: [
     {
-      number: 4,
-      x: 2,
-      y: 1,
-      answer: "BAROQUE",
-      clue: "Art and architectural style characterized by grandeur and exquisite detailing, originated in 17th century Europe.",
-      /* hints: [2, 7] */
-    },
-    {
       number: 1,
       x: 4,
       y: 1,
       answer: "DADA",
-      clue: "Characterized by artistic anarchy and humour, Man Ray and Marcel Duchamp were associated with this movement.",
+      clue: "1. Characterized by artistic anarchy and humour, Man Ray and Marcel Duchamp were associated with this movement.",
+      /* hints: [2, 7] */
+    },
+    {
+      number: 4,
+      x: 2,
+      y: 1,
+      answer: "BAROQUE",
+      clue: "4. Art and architectural style characterized by grandeur and exquisite detailing, originated in 17th century Europe.",
       /* hints: [2, 7] */
     },
     {
@@ -51,7 +51,7 @@ let definition = new CrosswordDefinition({
       x: 6,
       y: 4,
       answer: "TATE",
-      clue: "Museums located in a number of places in Britain including Liverpool and London.",
+      clue: "5. Museums located in a number of places in Britain including Liverpool and London.",
       /* hints: [2, 7] */
     },
     {
@@ -59,7 +59,7 @@ let definition = new CrosswordDefinition({
       x: 8,
       y: 1,
       answer: "OPART",
-      clue: "Victor Vasarely and Bridget Riley were associated with this art style which uses optical illusions and abstract patterns.",
+      clue: "6. Victor Vasarely and Bridget Riley were associated with this art style which uses optical illusions and abstract patterns.",
       /* hints: [2, 7] */
     }
   ],
